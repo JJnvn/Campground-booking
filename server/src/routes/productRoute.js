0fa583ae0be578f5cbf96906eb3e410c8eaa9e2c
Alarm24 <alarmirl24@gmail.com>
2025-04-25 06:53:18 +0700

    addCertAndChangePath

--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 const authMiddleware = require("../middleware/auth");
+const clientCert = require("../middleware/clientCert");
 
 router.use(authMiddleware);
+// router.use(clientCert); // new PKI auth (only for /api/products)
 
-router.get("/", productController.getAllProducts);
+router.get("/", clientCert, productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 router.post("/create", productController.createProduct);
 router.put("/update/:id", productController.updateProduct);
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,32 @@
 const dotenv = require("dotenv");
 const app = require("./app");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+// const logger = require("./utils/logger");
+
+dotenv.config({ path: "./src/config/config.env" });
+connectDB();
+
+// app.listen(PORT, () => {
+//   logger.info("Server is starting...");
+// });
+const https = require("https");
+const fs = require("fs");
 const logger = require("./utils/logger");
 
 dotenv.config({ path: "./src/config/config.env" });
 connectDB();
 
-app.listen(PORT, () => {
-    logger.info("Server is starting...");
-});
+const httpsOpts = {
+  key: fs.readFileSync("./src/certs/server.key"),
+  cert: fs.readFileSync("./src/certs/server.crt"),
+  ca: fs.readFileSync("./src/certs/rootCA.pem"),
+  // ---- client-cert auth:
+  requestCert: true,
+  rejectUnauthorized: true, // bounce unknown certificates
+};
+
+const PORT = process.env.PORT || 5003;
+https
+  .createServer(httpsOpts, app)
+  .listen(PORT, () => logger.info(`HTTPS server on https://localhost:${PORT}`));
